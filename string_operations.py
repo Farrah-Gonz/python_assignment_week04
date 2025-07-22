3de96c1eb91c096de9540aa6c394c468a474212d
Farrah-Gonz <farrahshaneszgonzales@gmail.com>
2025-07-22 10:48:12 +0700

    Update string_operations.py

--- string_operations.py
+++ string_operations.py
@@ -6,3 +6,13 @@ def lowercase_string(s):
     return s.lower()
 def uppercase_string(s):
     return s.upper()
+
+import string_operations as so
+
+sample_string = "hello World"
+
+print("Original:", sample_string)
+print("Reversed:", so.reverse_string(sample_string))
+print("Capitalized:", so.capitalize_string(sample_string))
+print("Lowercase:", so.lowercase_string(sample_string))
+print("Uppercase:", so.uppercase_string(sample_string))
